c85e1b40f042270a485628c0b7b1524804736c35
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-07-21 13:45:58 -0500

    CSS is working now

--- Web/App_Start/DurandalBundleConfig.cs
+++ Web/App_Start/DurandalBundleConfig.cs
@@ -1,38 +1,55 @@
 using System;
+using System.Collections.Generic;
 using System.Web.Optimization;
 
-namespace ProvenStyle.ReadEveryWord.Web {
-  public class DurandalBundleConfig {
-    public static void RegisterBundles(BundleCollection bundles) {
-      bundles.IgnoreList.Clear();
-      AddDefaultIgnorePatterns(bundles.IgnoreList);
-
-	  bundles.Add(
-		new ScriptBundle("~/Scripts/vendor")
-			.Include("~/Scripts/jquery-{version}.js")
-			.Include("~/Scripts/bootstrap.js")
-			.Include("~/Scripts/knockout-{version}.js")
-		);
-
-      bundles.Add(
-        new StyleBundle("~/Content/appcss")
-          .Include("~/Content/bootstrap.min.css")
-          .Include("~/Content/bootstrap.theme.min.css")
-		  .Include("~/Content/durandal.css")
-		  .Include("~/Content/book.css")
-        );
+namespace ProvenStyle.ReadEveryWord.Web
+{
+    class NonOrderingBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
     }
 
-    public static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
-      if(ignoreList == null) {
-        throw new ArgumentNullException("ignoreList");
-      }
+    public class DurandalBundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
 
-      ignoreList.Ignore("*.intellisense.js");
-      ignoreList.Ignore("*-vsdoc.js");
-      ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
-      //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
-      //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+            bundles.Add(
+              new ScriptBundle("~/Scripts/vendor")
+                  .Include("~/Scripts/jquery-{version}.js")
+                  .Include("~/Scripts/bootstrap.js")
+                  .Include("~/Scripts/knockout-{version}.js")
+              );
+
+            var bundle = new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.min.css")
+                .Include("~/Content/font-awesome.min.css")
+                .Include("~/Content/bootstrap-theme.min.css")
+                .Include("~/Content/durandal.css")
+                .Include("~/Content/starterkit.css")
+                .Include("~/Content/books.css");
+            bundle.Orderer = new NonOrderingBundleOrderer();
+
+            bundles.Add(bundle);
+        }
+
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
     }
-  }
 }
