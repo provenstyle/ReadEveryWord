cabba5c331e282fe0d8f17d550326ececc4c069b
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-07-20 22:13:32 -0500

    Wrote out all the books of the bible

---
+++ Web/Tests/describe_books.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Fakes;
+using Machine.Specifications;
+using ProvenStyle.ReadEveryWord.Web.Models;
+using Should;
+
+namespace Tests
+{
+    public class when_doing_something : WithSubject<History>
+    {
+        Establish context = () => { };
+
+        Because of = () => { };
+
+        It should_have_all_books = () => Subject.Books.Count().ShouldEqual(66);
+        It should_have_old_testament_books = () => Subject.OldTestamentBooks.Count().ShouldEqual(39);
+        It should_have_new_testament_books = () => Subject.NewTestamentBooks.Count().ShouldEqual(27);
+        It chapters_should_start_with_1 = () => Subject.Books.First().Chapters.First().Number.ShouldEqual(1);
+    }
+}
--- Web/Web/Data/Entities/ChapterRecord.cs
+++ Web/Web/Data/Entities/ChapterRecord.cs
@@ -4,7 +4,7 @@ namespace ProvenStyle.ReadEveryWord.Web.Data.Entities
 {
     public class ReadingRecord: BaseEntity
     {
-        public string Username { get; set; }
+        public string UserId { get; set; }
         public string Book { get; set; }
         public int Chapter { get; set; }
         public DateTime DateTime { get; set; }
