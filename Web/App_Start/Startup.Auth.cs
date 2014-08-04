a1d3bbd570ffcf055a089b27e1c056d6e0f2cbf6
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-04 10:48:57 -0500

    Fixing twitter certificate

--- Web/App_Start/Startup.Auth.cs
+++ Web/App_Start/Startup.Auth.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Twitter;
 using Owin;
 using System;
 using ProvenStyle.ReadEveryWord.Web.Models;
@@ -39,9 +41,19 @@ namespace ProvenStyle.ReadEveryWord.Web
             //    clientId: "",
             //    clientSecret: "");
 
-            app.UseTwitterAuthentication(
-               consumerKey: "7EvdQajZNPsWbZ32d30GCdaC0",
-               consumerSecret: "4zZ4q8m7Vm0FPUrMaH7sQjSlrHO18EuU7vIFb7sVX52dY231jH");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = "7EvdQajZNPsWbZ32d30GCdaC0",
+                ConsumerSecret = "4zZ4q8m7Vm0FPUrMaH7sQjSlrHO18EuU7vIFb7sVX52dY231jH", 
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(
+                new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD" // Symantec Class 3 Secure Server CA - G4
+                })
+            });
 
             //app.UseFacebookAuthentication(
             //   appId: "",
