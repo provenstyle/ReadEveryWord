fd60020ef493bfbc0051b515d1dd974232162390
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-16 16:56:44 -0500

    Adding a monitoring endpoint

---
+++ Web/Controllers/MonitorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using ProvenStyle.ReadEveryWord.Web.BaseTypes;
+
+namespace ProvenStyle.ReadEveryWord.Web.Controllers
+{
+    public class MonitorController : BaseApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
