0b9f2a6d3cebe97928c8bde8d502361683095faa
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-17 12:28:18 -0500

    Turning the login viewmodel into a constructor function

--- Web/App/viewmodels/login.js
+++ Web/App/viewmodels/login.js
@@ -1,53 +1,58 @@
 ﻿define(['durandal/system', 'plugins/observable', 'models/history', 'models/user'], function (system, observable, history, user) {
 
-    var vm = {},
-        validator = {};
+    var ctor = function() {
 
-    vm.email = '';
-    vm.password = '';
-    vm.loginError = false;
+        var self = this,
+            validator = {};
 
-    vm.create = function () {
-        var valid = validator.form();
-        system.log("Login form valid? " + valid);
-        if (valid === true) {
-            submitForm();
-        }
-    };
+        self.email = '';
+        self.password = '';
+        self.loginError = false;
 
-    vm.compositionComplete = function() {
-        validator = $("#login").validate();
-    };
+        self.create = function () {
+            var valid = validator.form();
+            system.log("Login form valid? " + valid);
+            if (valid === true) {
+                submitForm();
+            }
+        };
 
-    function submitForm() {
-        $.post(rew.config.basePath() + '/api/accountApi/login', {
-            email: vm.email,
-            password: vm.password
-        })
-            .done(function (data, status, xhr) {
-                system.log('Login successfull');
-                user.authenticated(data.username);
-                history.prime()
-                    .done(function () {
-                        location.hash = "#books";
-                    });
-            })
-            .fail(function () {
-                system.log('Failed to login. Invalid username or password.');
-                vm.loginError = true;
-                user.clear();
-           });
-    };
+        self.compositionComplete = function () {
+            validator = $("#login").validate();
+        };
 
-    function resetError() {
-        vm.loginError = false;
-    };
-
-    _.each(['email', 'password'], function (prop) {
-        observable(vm, prop).subscribe(function () {
-            resetError();
+        _.each(['email', 'password'], function (prop) {
+            observable(self, prop).subscribe(function () {
+                resetError();
+            });
         });
-    });
 
-    return vm;
+        function submitForm() {
+            $.post(rew.config.basePath() + '/api/accountApi/login', {
+                email: self.email,
+                password: self.password
+            })
+                .done(function (data, status, xhr) {
+                    system.log('Login successfull');
+                    user.authenticated(data.username);
+                    history.prime()
+                        .done(function () {
+                            location.hash = "#books";
+                        });
+                })
+                .fail(function () {
+                    system.log('Failed to login. Invalid username or password.');
+                    self.loginError = true;
+                    user.clear();
+                });
+        };
+
+        function resetError() {
+            self.loginError = false;
+        };
+
+    };
+
+    return ctor;
+
 });
