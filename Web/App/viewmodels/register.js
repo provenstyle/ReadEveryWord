570f5914dc9846b09bdd14b3940f88f945b934ac
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-17 13:09:35 -0500

    Turning registration viewModel into a constructor function

--- Web/App/viewmodels/register.js
+++ Web/App/viewmodels/register.js
@@ -1,77 +1,79 @@
 ﻿define(['durandal/system', 'plugins/observable', 'models/history', 'models/user'], function (system, observable, history, user) {
 
-    var vm = {},
-        validator = {};
+    var ctor = function() {
+        var self = this,
+            validator = {};
 
-    vm.email = '';
-    vm.password = '';
-    vm.confirmPassword = '';
-    vm.registrationError = false;
+        self.email = '';
+        self.password = '';
+        self.confirmPassword = '';
+        self.registrationError = false;
 
-    vm.compositionComplete = function () {
-        validator = $("#register").validate({
-            rules: {
-                newPassword: {
-                    validpassword: true
-                },
-                newEmail: {
-                    required: true,
+        self.compositionComplete = function () {
+            validator = $("#register").validate({
+                rules: {
+                    newPassword: {
+                        validpassword: true
+                    },
+                    newEmail: {
+                        required: true,
 
-                    remote: {
-                        url: rew.config.basePath() + "/api/accountApi/EmailAvailable",
-                        type: "get",
-                        data: {
-                            email: function() {
-                                return vm.email;
+                        remote: {
+                            url: rew.config.basePath() + "/api/accountApi/EmailAvailable",
+                            type: "get",
+                            data: {
+                                email: function () {
+                                    return self.email;
+                                }
                             }
                         }
                     }
+                },
+                messages: {
+                    newEmail: {
+                        required: "This field is required.",
+                        remote: "Email address already in use."
+                    }
                 }
-            },
-            messages: {
-                newEmail: {
-                    required: "This field is required.",
-                    remote: "Email address already in use."
-                }
+            });
+
+        };
+
+        self.create = function () {
+            var valid = validator.form();
+            system.log("Registration form valid? " + valid);
+            if (valid) {
+                $.post(rew.config.basePath() + '/api/accountApi/register', {
+                    email: self.email,
+                    password: self.password,
+                    confirmPassword: self.confirmPassword
+                })
+                    .done(function () {
+                        system.log('New user created.');
+                        user.authenticated(self.email);
+                        history.prime()
+                            .done(function () {
+                                location.hash = "#books";
+                            });
+                        location.hash = "#books";
+                    })
+                    .fail(function (result) {
+                        system.log('Failed to create account: ' + result.status);
+                        self.registrationError = true;
+                    });
             }
+        };
+
+        _.each(['email', 'password', 'confirmPassword'], function (prop) {
+            observable(self, prop).subscribe(function () {
+                resetError();
+            });
         });
 
+        function resetError() {
+            self.registrationError = false;
+        };
     };
 
-    vm.create = function () {
-        var valid = validator.form();
-        system.log("Registration form valid? " + valid);
-        if (valid) {
-            $.post(rew.config.basePath() + '/api/accountApi/register', {
-                    email: vm.email,
-                    password: vm.password,
-                    confirmPassword: vm.confirmPassword
-                })
-                .done(function() {
-                    system.log('New user created.');
-                    user.authenticated(vm.email);
-                    history.prime()
-                        .done(function() {
-                            location.hash = "#books";
-                        });
-                    location.hash = "#books";
-                })
-                .fail(function(result) {
-                    system.log('Failed to create account: ' + result.status);
-                    vm.registrationError = true;
-                });
-        }
-    };
-
-    function resetError() {
-        vm.registrationError = false;
-    };
-
-    _.each(['email', 'password', 'confirmPassword'], function(prop) {
-        observable(vm, prop).subscribe(function () {
-            resetError();
-        });
-    });
-
-    return vm;
+    return ctor;
 });
