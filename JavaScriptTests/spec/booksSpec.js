3859e39931027c737f583cb33de3c092853a6632
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-23 14:02:33 -0500

    Moved all book tests to jasmine

--- JavaScriptTests/spec/booksSpec.js
+++ JavaScriptTests/spec/booksSpec.js
@@ -23,4 +23,37 @@ describe('books', function () {
         expect(books.newTestamentBooks.length).toEqual(27);
     });
 
+    it('chapters should start with 1', function () {
+        expect(books.newTestamentBooks[0].chapters[0].number).toEqual(1);
+    });
+
+    it('should have unique short names', function () {
+        var allBooks = {},
+            book,
+            i;
+
+        for (i = 0; i < 39; i++) {
+            book = books.oldTestamentBooks[i];
+            allBooks[book.shortName] = book;
+        }
+
+        for (i = 0; i < 27; i++) {
+            book = books.newTestamentBooks[i];
+            allBooks[book.shortName] = book;
+        }
+
+        var bookCount = propertyCount(allBooks);
+        expect(bookCount).toEqual(66);
+
+        function propertyCount(obj) {
+            var size = 0, key;
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++;
+            }
+            return size;
+        }
+
+    });
+
+
 });
