b0c5f023de59ee371907dac065898d21ad7f3cfc
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-23 12:47:14 -0500

    Really testing the books now

--- JavaScriptTests/spec/booksSpec.js
+++ JavaScriptTests/spec/booksSpec.js
@@ -1,11 +1,23 @@
 ﻿describe('books', function() {
+    var books;
 
-    it('should exist', function(cb) {
-        require(['models/books'], function(Books) {
-            var books = new Books();
-            expect(books).toBeDefined();
+    beforeEach(function(cb) {
+        require(['models/books'], function (Books) {
+            books = new Books();
             cb();
         });
+    });
+
+    it('should exist', function() {
+        expect(books).toBeDefined();
+    });
+
+    it('should have the 39 old testament books', function () {
+        expect(books.oldTestamentBooks.length).toEqual(39);
+    });
+
+    it('should have the 27 new testament books', function () {
+        expect(books.newTestamentBooks.length).toEqual(27);
     });
 
 });
