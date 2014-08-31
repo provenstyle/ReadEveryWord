5d07393863fd8e6364ad8723bbece65b01138099
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-31 14:47:22 -0500

    Built for windows 8

---
+++ cordova/platforms/windows8/www/App/models/chapter.js
@@ -0,0 +1,8 @@
+﻿define(function() {
+    var ctor = function(number) {
+        this.number = number;
+        this.read = false;
+    };
+
+    return ctor;
+});
---
+++ cordova/platforms/windows8/www/Scripts/config.js
@@ -0,0 +1,9 @@
+﻿(function(rew) {
+    rew.config = {
+        basePath: function() {
+            //return location.protocol + '//' + location.host; //from the web
+            return 'https://readeveryword.azurewebsites.net'; //embedded
+        }
+    }
+
+})(window.rew = window.rew || {});
