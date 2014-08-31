4192da9a2f91d2ed4e3d3a3bf74e47425c8d155e
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-31 16:51:13 -0500

    Deleted files

--- Web/cordovaConfig.js
+++
@@ -1,9 +0,0 @@
-﻿(function(rew) {
-    rew.config = {
-        basePath: function() {
-            return location.protocol + '//' + location.host; //from the web
-            //return 'https://ReadEveryWord'; //embedded
-        }
-    }
-
-})(window.rew = window.rew || {});
