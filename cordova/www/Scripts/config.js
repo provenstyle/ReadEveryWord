a719f2d3133544871a9ce60f32d89b0c588ff92a
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-31 08:20:51 -0500

    pointing to azure

--- cordova/www/Scripts/config.js
+++ cordova/www/Scripts/config.js
@@ -2,7 +2,7 @@
     rew.config = {
         basePath: function() {
             //return location.protocol + '//' + location.host; //from the web
-            return 'https://ReadEveryWord.com'; //embedded
+            return 'https://readeveryword.azurewebsites.net'; //embedded
         }
     }
 
