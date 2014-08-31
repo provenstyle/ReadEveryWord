1034a82493074a0044254ed9cf830817624305f9
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-31 15:20:32 -0500

    Working on ios

---
+++ cordova/platforms/ios/www/App/models/history.js
@@ -0,0 +1,73 @@
+﻿define(['durandal/system', 'services/history', 'models/books'], function (system, service, booksCtor) {
+
+    var model = {};
+
+    model.prime = function () {
+        var books = new booksCtor();
+
+        model.oldTestament = books.oldTestamentBooks;
+        model.newTestament = books.newTestamentBooks;
+        model.bookIndex = books.bookIndex;
+
+        return service.getHistory()
+            .done(function (data) {
+
+                model.historyRecords = data;
+
+                for (var i = 0; i < data.years.length; i++) {
+                    var year = data.years[i];
+                    for (var j = 0; j < year.months.length; j++) {
+                        var month = year.months[j];
+                        for (var k = 0; k < month.days.length; k++) {
+                            var day = month.days[k];
+
+                                var book = model.bookIndex[day.book];
+                                var chapter = book.chapters[day.chapter - 1];
+                                chapter.read = true;
+                        }
+                    }
+                }
+            });
+    };
+
+    model.bookByName = function (bookName) {
+        return model.bookIndex[bookName];
+    };
+
+    model.clear = function () {
+        model.oldTestament = [];
+        model.newTestament = [];
+        model.bookIndex = [];
+        system.log("Cleared reading history.");
+    };
+
+    model.addToHistoryRecords = function (data) {
+
+        if (model.historyRecords.years.length === 0) { return; }
+
+        model.historyRecords.years[0].months[0].days.unshift({
+            book: data.book,
+            chapter: data.chapter,
+            day: new Date().getDate()
+        });
+    };
+
+    model.removeHistoryRecords = function(data) {
+        for (var i = 0; i < model.historyRecords.years.length; i++) {
+            var year = model.historyRecords.years[i];
+            for (var j = 0; j < year.months.length; j++) {
+                var month = year.months[j];
+                for (var k = 0; k < month.days.length; k++) {
+                    var day = month.days[k];
+                    if (day.book === data.book && day.chapter === data.chapter) {
+                        month.days.splice(k, 1);
+                        return;
+                    }
+                }
+            }
+        }
+    };
+
+    return model;
+
+});
---
+++ cordova/platforms/ios/www/App/services/history.js
@@ -0,0 +1,47 @@
+﻿define(['durandal/system'], function (system) {
+
+    return {
+        getHistory: function () {
+            var dfd = new $.Deferred();
+
+            $.ajax({
+                url: rew.config.basePath() + '/api/history',
+                xhrFields: {
+                    withCredentials: true
+                }
+            })
+                .done(function (data) {
+                    dfd.resolve(data);
+                })
+                .fail(function () {
+                    toastr.error('Unable to get reading history.');
+                });
+
+            return dfd;
+        },
+        postHistory: function(bookShortName, chapter, read) {
+
+            var data = {
+                book: bookShortName,
+                chapter: chapter,
+                read: read
+            };
+
+            return $.ajax({
+                type: 'POST',
+                url: rew.config.basePath() + '/api/history',
+                data: data,
+                xhrFields: {
+                    withCredentials: true
+                }
+            })
+            .done(function () {
+                    system.log("Successfully updated history" + bookShortName + " " + chapter + " " + read);
+                })
+            .fail(function (xhr) {
+                system.log("Failed to save ReadRecord: " + xhr.status);
+                toastr.error("Unable to save.");
+            });
+        }
+    };
+});
