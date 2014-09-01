9a44ca00bca7e7f966e907672d09847df3053dc2
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-09-01 15:56:42 -0500

    updating cordova to the production subdomain

--- Web/cordova/cordovaConfig.js
+++ Web/cordova/cordovaConfig.js
@@ -2,7 +2,7 @@
     rew.config = {
         basePath: function() {
             //return location.protocol + '//' + location.host; //from the web
-            return 'https://readeveryword.azurewebsites.net'; //cordova
+            return 'https://bible.readeveryword.com'; //cordova
         }
     }
 
