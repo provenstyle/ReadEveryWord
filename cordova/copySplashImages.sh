dee10068e23d707e20deeef5dc59e0dcef99ca23
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-09-06 16:14:51 -0500

    icon mess

--- cordova/copySplashImages.sh
+++ cordova/copySplashImages.sh
@@ -7,10 +7,10 @@ cp $SRC/screen-ipad-landscape.png $DEST/Default-Landscape~ipad.png
 cp $SRC/screen-ipad-portrait-2x.png $DEST/Default-Portrait@2x~ipad.png
 cp $SRC/screen-ipad-portrait.png $DEST/Default-Portrait~ipad.png
 
-cp $SRC/screen-ipad-portrait-prior.png $DEST/Default-Portrait-Prior~ipad.png
-cp $SRC/screen-ipad-portrait-prior-2x.png $DEST/Default-Portrait-Prior-2x~ipad.png
-cp $SRC/screen-ipad-landscape-prior.png $DEST/Default-Landscape-Prior~ipad.png
-cp $SRC/screen-ipad-landscape-prior-2x.png $DEST/Default-Landscape-Prior-2x~ipad.png
+cp $SRC/Default-Portrait.png $DEST/Default-Portrait.png
+cp $SRC/Default-Portrait@2x.png $DEST/Default-Portrait@2x.png
+cp $SRC/Default~ipad.png $DEST/Default~ipad.png
+cp $SRC/Default.png $DEST/Default.png
 
 
 
