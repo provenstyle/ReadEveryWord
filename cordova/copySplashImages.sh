65c15774b29fa87497929f266d142f89623ba02d
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-09-06 15:45:26 -0500

    new images

--- cordova/copySplashImages.sh
+++ cordova/copySplashImages.sh
@@ -6,5 +6,13 @@ cp $SRC/screen-ipad-landscape-2x.png $DEST/Default-Landscape@2x~ipad.png
 cp $SRC/screen-ipad-landscape.png $DEST/Default-Landscape~ipad.png
 cp $SRC/screen-ipad-portrait-2x.png $DEST/Default-Portrait@2x~ipad.png
 cp $SRC/screen-ipad-portrait.png $DEST/Default-Portrait~ipad.png
+
+cp $SRC/screen-ipad-portrait-prior.png $DEST/Default-Portrait-Prior~ipad.png
+cp $SRC/screen-ipad-portrait-prior-2x.png $DEST/Default-Portrait-Prior-2x~ipad.png
+cp $SRC/screen-ipad-landscape-prior.png $DEST/Default-Landscape-Prior~ipad.png
+cp $SRC/screen-ipad-landscape-prior-2x.png $DEST/Default-Landscape-Prior-2x~ipad.png
+
+
+
 cp $SRC/screen-iphone-portrait-2x.png $DEST/Default@2x~iphone.png
 cp $SRC/screen-iphone-portrait.png $DEST/Default~iphone.png
