2bb36e25b05471b5c35cdaa25b23826ee566629e
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-10-25 13:15:11 -0500

    overwriting angulars default email validation

---
+++ Web/App/directives/overwrite-email.js
@@ -0,0 +1,18 @@
+﻿angular.module('readEveryWord')
+    .directive('overwriteEmail', function () {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function (scope, elm, attrs, ctrl) {
+                // only apply the validator if ngModel is present and Angular has added the email validator
+                if (ctrl && ctrl.$validators.email) {
+
+                    // this will overwrite the default Angular email validator
+                    ctrl.$validators.email = function (modelValue) {
+                        var EMAIL_REGEXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                    };
+                }
+            }
+        };
+    });
