24310f4d2a554cd0020b771280fe0dc99b1cb574
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-10-25 17:43:44 -0500

    formatting

--- Web/App/directives/unique-email.js
+++ Web/App/directives/unique-email.js
@@ -1,14 +1,23 @@
-﻿angular.module('readEveryWord')
-    .directive('uniqueEmail', function () {
+﻿(function() {
+    angular
+        .module('readEveryWord')
+        .directive('uniqueEmail', uniqueEmail);
+
+    uniqueEmail.$inject = ['accountService'];
+
+    function uniqueEmail (accountService) {
         return {
             require: 'ngModel',
             restrict: 'A',
             link: function (scope, elm, attrs, ctrl) {
                 if (ctrl) {
                     ctrl.$asyncValidators.uniqueEmail = function (modelValue) {
-                        
+                        return accountService.uniqueEmail(modelValue)
+                            .then();
                     };
                 }
             }
         };
-    });
+    }
+})();
+
