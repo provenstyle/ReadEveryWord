f3ed5ef81e88f59c6f8c6090799aab980a9a98c2
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-11-07 03:35:59 -0600

    displaying list of books

--- Web/App/controllers/booksController.js
+++ Web/App/controllers/booksController.js
@@ -3,9 +3,14 @@
         .module('readEveryWord')
         .controller('booksController', booksController);
 
-    booksController.$inject = [];
+    booksController.$inject = ['$scope', '$location', 'userModel', 'historyModel'];
 
-    function booksController() {
-        
+    function booksController($scope, $location, userModel, historyModel) {
+      
+        $scope.books = historyModel.books;
+
+        $scope.toggleRead = function (chapter) {
+            $location.path('/chapters/' + +encodeURIComponent(chapter.shortName));
+        };
     }
 })();
