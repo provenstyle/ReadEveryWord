f3e406751d2fab3f07d8e6252d3e79f02958461a
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-11-08 15:08:49 -0800

    Hiding the mobile menu on click

---
+++ Web/App/directives/mobile-nav-fix.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    angular
+        .module('readEveryWord')
+        .directive('mobileNavFix', mobileNavFix);
+
+    mobileNavFix.$inject = [];
+
+    function mobileNavFix() {
+        return {
+            restrict: 'A',
+            link: function(scope, elm, attrs, ctrl) {
+                elm.on('click', function () {
+                    closeMobileNav();
+                });
+
+                $('.navbar-brand').on('click', function () {
+                    closeMobileNav();
+                });
+
+                function closeMobileNav() {
+                    var smallMenu = $('.navbar-header .navbar-toggle');
+
+                    if (smallMenu.hasClass('collapsed') !== true) {
+                        smallMenu.trigger("click");
+                    }
+                }
+            }
+
+        }        
+    }
+})();
