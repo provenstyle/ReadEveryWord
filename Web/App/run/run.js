87e166fb898d9681caf7c12bcc8b465ee415957a
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-11-09 15:42:28 -0800

    configuring toastr

--- Web/App/run/run.js
+++ Web/App/run/run.js
@@ -3,12 +3,13 @@
         .module('readEveryWord')
         .run(run);
 
-    run.$inject = ['$log', 'bootstrapper', 'routeEvents'];
+    run.$inject = ['$log', 'bootstrapper', 'routeEvents', 'toastrConfig'];
 
-    function run($log, bootstrapper, routeEvents) {
+    function run($log, bootstrapper, routeEvents, toastrConfig) {
         $log.debug('*** run');
         bootstrapper.initialize();
         routeEvents.initialize();
+        toastrConfig.initialize();
     }
 
 })();
---
+++ Web/App/run/toastrConfig.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    angular
+        .module('readEveryWord')
+        .factory('toastrConfig', toastrConfig);
+
+    function toastrConfig() {
+        return {
+            initialize: initialize
+        }
+
+        function initialize() {
+            toastr.options = {
+                timeOut: 1000
+            };
+        }
+    }
+})();
