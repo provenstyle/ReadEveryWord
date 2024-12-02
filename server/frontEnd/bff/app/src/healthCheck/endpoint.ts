83127e15845186157b849194cd6891bff1755b31
michael dudley <michael.dudley@improving.com>
2024-12-02 09:41:13 -0600

    stubbing out a bff

---
+++ server/frontEnd/bff/app/src/healthCheck/endpoint.ts
@@ -0,0 +1,22 @@
+import { app, type HttpRequest, type HttpResponseInit, type InvocationContext } from '@azure/functions'
+
+app.http('health_check', {
+  methods: ['GET'],
+  authLevel: 'function',
+  handler: handleEndpoint,
+  route: 'healthCheck'
+})
+
+export async function handleEndpoint (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  try {
+    console.log(`${request.method} request for url "${request.url}"`)
+
+    return {
+        status: 200,
+    }
+
+  } catch (error) {
+    console.error(`Error handling "${request.url}"`, error)
+    return ({ status: 500 })
+  }
+}
