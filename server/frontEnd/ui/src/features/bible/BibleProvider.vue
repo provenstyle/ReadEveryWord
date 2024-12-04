59c6f158cfb7b9215671893a294c1db4925b75d0
michael dudley <michael.dudley@improving.com>
2024-12-03 19:20:36 -0600

    configuring swa cli

--- server/frontEnd/ui/src/features/bible/BibleProvider.vue
+++ server/frontEnd/ui/src/features/bible/BibleProvider.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import { Bible } from '@read-every-word/domain'
 import { provide, reactive } from 'vue'
+import axios from 'axios'
 
 const bible = reactive(new Bible())
 
 provide('bible', bible)
+
+console.log('**********')
+axios.get('/api/healthCheck')
+.then((r) => {
+  console.log(r)
+})
+.catch((e) => {
+  console.log(e)
+})
+
 </script>
 
 <template>
