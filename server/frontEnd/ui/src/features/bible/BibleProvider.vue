e1c6a2ee0a50185fed1e4e1d99293a75befd2572
michael dudley <michael.dudley@improving.com>
2024-12-05 23:43:44 -0600

    attach a bearer token

--- server/frontEnd/ui/src/features/bible/BibleProvider.vue
+++ server/frontEnd/ui/src/features/bible/BibleProvider.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue'
 import { Bible } from '@read-every-word/domain'
 import { provide, reactive } from 'vue'
 import axios from 'axios'
 import { onMounted } from 'vue'
 
+const auth = useAuth0()
 const bible = reactive(new Bible())
 
 provide('bible', bible)
 
-onMounted(() => {
-  console.log('**********')
-  axios.get('/api/healthCheck')
-  .then((r) => {
-    console.log(r)
-  })
-  .catch((e) => {
-    console.log(e)
-  })
+onMounted(async () => {
+  const token = await auth.getAccessTokenSilently()
+  const axiosConfig = {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+  }
+
+  axios.get('/api/healthCheck', axiosConfig)
+    .then((r) => {
+      console.log(r)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
 })
 
 </script>
