31bcf2dcb04a644802879a17242f0ab56b5e1bf6
michael dudley <michael.dudley@improving.com>
2025-01-30 10:27:26 -0600

    deploying dev

--- server/api/cicd/variables.sh
+++ server/api/cicd/variables.sh
@@ -1,13 +1,15 @@
 SERVICE_NAME=read-every-word-api
 
+# Example state key
+# STATE_KEY="myEnvironment/myBranch/myPipeline/myName/default/myService/terraform.tfstate"
+
 # Dev Variables
-#ENVIRONMENT=dev
-#STATE_KEY="myEnvironment/myBranch/myPipeline/myName/default/myService/terraform.tfstate"
-#STATE_KEY="$ENVIRONMENT/fresh-start/manual/mpd/default/$SERVICE_NAME/terraform.tfstate"
-#FUNCTION_APP_NAME=fa-ddixmmxvxydnaklsnd
+ENVIRONMENT=dev
+STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
+FUNCTION_APP_NAME=fa-cmvkmwjkuwgxfhrelc
 
 # Prod Variables
-ENVIRONMENT=prod
-STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
-FUNCTION_APP_NAME=fa-oyckxrbdmprrhejrrq
+# ENVIRONMENT=prod
+# STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
+# FUNCTION_APP_NAME=fa-oyckxrbdmprrhejrrq
 
--- server/frontEnd/cicd/variables.sh
+++ server/frontEnd/cicd/variables.sh
@@ -1,14 +1,15 @@
 SERVICE_NAME=read-every-word-front-end
 
-# dev variables
-# ENVIRONMENT=dev
 # STATE_KEY="myEnvironment/myBranch/myPipeline/myName/default/myService/terraform.tfstate"
-# STATE_KEY="$ENVIRONMENT/fresh-start/manual/mpd/default/$SERVICE_NAME/terraform.tfstate"
-# FUNCTION_APP_NAME=fa-lpbtpdxqjquvoitmpr
-# STATIC_APP_NAME=stapp-aawnmnwbvsulhxsnwu
+
+# dev variables
+ENVIRONMENT=dev
+STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
+FUNCTION_APP_NAME=fa-rpbpnabjedhwlhvhwu
+STATIC_APP_NAME=stapp-ehmrwpxjociwslfudu
 
 # prod variables
-ENVIRONMENT=prod
-STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
-FUNCTION_APP_NAME=fa-glsuwfipqqitmbugqr
-STATIC_APP_NAME=stapp-xgnsvotromajjgexni
+# ENVIRONMENT=prod
+# STATE_KEY="$ENVIRONMENT/main/default/$SERVICE_NAME/terraform.tfstate"
+# FUNCTION_APP_NAME=fa-glsuwfipqqitmbugqr
+# STATIC_APP_NAME=stapp-xgnsvotromajjgexni
