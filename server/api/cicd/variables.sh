b91d1a14bc8802ceadfb5cdb2540bb60bc491ffe
michael dudley <michael.dudley@improving.com>
2025-02-05 23:10:03 -0600

    parameterizing the type of build

--- server/api/cicd/variables.sh
+++ server/api/cicd/variables.sh
@@ -1,4 +1,4 @@
 set -e
 
 SERVICE_NAME=read-every-word-api
-STATE_KEY="$ENVIRONMENT/$BRANCH/default/$SERVICE_NAME/terraform.tfstate"
+STATE_KEY="$ENVIRONMENT/$BRANCH/$TYPE/$SERVICE_NAME/terraform.tfstate"
--- server/frontEnd/cicd/variables.sh
+++ server/frontEnd/cicd/variables.sh
@@ -1,4 +1,4 @@
 set -e
 
 SERVICE_NAME=read-every-word-front-end
-STATE_KEY="$ENVIRONMENT/$BRANCH/default/$SERVICE_NAME/terraform.tfstate"
+STATE_KEY="$ENVIRONMENT/$BRANCH/$TYPE/$SERVICE_NAME/terraform.tfstate"
