c225efd76c8ee9593e052692b15fb81bbffdd989
michael dudley <michael.dudley@improving.com>
2025-02-10 17:50:39 -0600

    putting remote state key back in place

--- server/frontEnd/terraform/remote_state.tf
+++ server/frontEnd/terraform/remote_state.tf
@@ -8,6 +8,6 @@ data "terraform_remote_state" "api" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
     container_name       = var.state_container_name
-    key                  = var.state_key
+    key = replace(var.state_key, "/read-every-word-front-end/terraform.tfstate", "/read-every-word-api/terraform.tfstate")
   }
 }
