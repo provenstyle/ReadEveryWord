38d021039a128c5d92e41c140a153b6ce4134a33
michael dudley <michael.dudley@improving.com>
2025-10-12 18:21:33 -0500

    stop the cli from changing the configuration

--- server/frontEnd/cicd/publishFrontend.sh
+++ server/frontEnd/cicd/publishFrontend.sh
@@ -7,8 +7,12 @@ cd ../ui
 npm run build
 
 npx swa login
+
+# workaround to keep the cli from modifying the config file
+chmod 444 swa-cli.config.json
 npx swa deploy                       \
     --resource-group $RESOURCE_GROUP \
     --app-name $STATIC_APP_NAME      \
     --app-location ./dist            \
     --env production
+chmod 644 swa-cli.config.json
