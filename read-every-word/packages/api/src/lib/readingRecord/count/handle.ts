5f32cc0cac41ff704886ac1dd3e583cf71e164ef
michael dudley <michael.dudley@improving.com>
2025-11-18 17:09:07 -0600

    removing zod

--- read-every-word/packages/api/src/lib/readingRecord/count/handle.ts
+++ read-every-word/packages/api/src/lib/readingRecord/count/handle.ts
@@ -1,15 +1,17 @@
 import { isErr, ok } from '@read-every-word/foundation'
-import {type CountReadingRecordResult} from '@read-every-word/domain'
+import {CountReadingRecord, type CountReadingRecordResult} from '@read-every-word/domain'
 import { Persistence } from './persistence.js'
 import { authenticatedProcedure } from '../../trpc.js'
-import { z } from 'zod'
+import { validate } from './validation.js'
 
 export const countReadingRecordProcedure = authenticatedProcedure
-  .input(z.object({
-    authId: z.string(),
-    readingCycleId: z.uuid()
-  }))
+  .input(r => r as CountReadingRecord)
   .query(async ({ input, ctx }): Promise<CountReadingRecordResult> => {
+    const validationResponse = await validate(input)
+    if(isErr(validationResponse)) {
+      return validationResponse
+    }
+
     const persistence = new Persistence(ctx.config)
     const countReadingRecordResponse = await persistence.countReadingRecords(input)
     if (isErr(countReadingRecordResponse)) {
