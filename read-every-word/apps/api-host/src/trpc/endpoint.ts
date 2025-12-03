a2f60189d826db3bf43ad38fc113eb29896fcfbf
michael dudley <michael.dudley@improving.com>
2025-12-03 12:49:44 -0600

    trying to add an endpoint

---
+++ read-every-word/apps/api-host/src/trpc/endpoint.ts
@@ -0,0 +1,15 @@
+import { app, type HttpRequest, type HttpResponseInit, type InvocationContext } from '@azure/functions'
+import { appRouter } from '@read-every-word/api'
+
+app.http('get_health_check', {
+    methods: ['GET'],
+    authLevel: 'function',
+    handler: handleEndpoint,
+    route: 'healthCheck'
+})
+
+export async function handleEndpoint (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    return {
+        status: 200
+    }
+}
